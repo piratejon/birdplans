a423602a39716fe8b63558748048d717910fd40e
Jonathan Wesley Stone <js@piratejon.com>
2018-11-21 07:28:03 -0600

    getting started

--- birdplan.py
+++ birdplan.py
@@ -12,6 +12,8 @@ import unittest
 
 from skyfield.api import load, Topos
 import maidenhead as mh
+from scipy import optimize
+import numpy as np
 
 class TestBirdplan(unittest.TestCase):
     '''exercise the various functions in birdplan'''
@@ -22,10 +24,10 @@ class TestBirdplan(unittest.TestCase):
 
         # check initialization stuff
         self.assertEqual(plan.tle_file, 'data/tle/amateur.txt')
-        self.assertTrue('FOX-1B' in plan.tle)
+        self.assertTrue('SO-50' in plan.tle)
 
         # find next 24hrs passes
-        results = plan.query_pass(
+        results = plan.query_point_in_time(
             'SO-50', 'EM15ek', plan.timescale.utc(2018, 11, 19, 2, 11, 0)
         )
         self.assertAlmostEqual(results.latlng[0], 35.41667, places=5)
@@ -33,10 +35,10 @@ class TestBirdplan(unittest.TestCase):
         self.assertAlmostEqual(results.alt.degrees, 26.71944, places=5)
         self.assertAlmostEqual(results.azimuth.degrees, 196.69795, places=5)
         self.assertAlmostEqual(results.distance, 1244.45630, places=5)
-        #self.assertEqual(results.point[0].t, (2018, 11, 19, 2, 6, 33))
-        #self.assertEqual(results.point[0].az, 200)
-        #self.assertEqual(results.point[-1].t, (2018, 11, 19, 2, 21, 12))
-        #self.assertEqual(results.point[-1].az, 30)
+
+        results = plan.query_pass(
+            'SO-50', 'EM15ek', plan.timescale.utc(2018, 11, 19, 0, 0, 0), 24, 15
+        )
 
 class BirdplanResults:
     '''results of a birdplan query'''
@@ -47,6 +49,10 @@ class BirdplanResults:
         self.alt, self.azimuth, self.distance = topocentric.altaz()
         self.distance = self.distance.km
 
+def f_altitude(bird, topos, time_t):
+    '''calculate the altitude of bird with respect to topos at time_t'''
+    (bird - topos).at(time_t).topocentric.altaz()[0]
+
 class Birdplan:
     '''interface for bird planning'''
 
@@ -56,9 +62,32 @@ class Birdplan:
         self.tle = load.tle(tle_file)
         self.timescale = load.timescale()
 
-    def query_pass(self, bird, maidenhead, when):
+    def query_point_in_time(self, bird, maidenhead, when):
         '''find the altitude, azimuth, and distance of bird with respect to maidenhead at time'''
         where = mh.toLoc(maidenhead)
         diff = self.tle[bird] - Topos(*where)
         topocentric = diff.at(when)
         return BirdplanResults(where, topocentric)
+
+    def query_pass(bird, maidenhead, start, dur_hrs, min_alt):
+        '''
+        try this:
+        <https://github.com/skyfielders/astronomy-notebooks/blob/master/Solvers/Earth-Satellite-Passes.ipynb>
+        as access 2018-11-19
+        '''
+        where = mh.toLoc(maidenhead)
+        diff = self.tle[bird] - Topos(*where)
+
+        orbit_period_per_minute = (2.0 * np.pi) / self.tle[bird].model.no
+        orbit_period = orbit_period_per_minute / 24.0 / 60.0
+        revolutions_per_day = 1.0 / orbit_period
+        step = orbit_period / 6
+        t0 = start.tai
+        t = t0 + np.arange(0, 1.0, step)
+        dt = JulianDate(tai=t).utc_datetime()
+        altitudes = f_altitude(self.tle[bird], Topos(*where), dt) # diff.at(dt).topocentric.altaz()[0]
+        left_diff = np.ediff1d(altitudes, to_begin=0.0)
+        right_diff = np.ediff1d(altitudes, to_end=0.0)
+        maxima = (left_diff > 0.0) & (right_diff < 0.0)
+
+        result = optimize.minimize_scalar(lambda t: -f_altitude(t), bracket=[t + step, t - step], tol=one_second / t)
