aea0bef58b4d9329aee637c61fe76c781bc10eec
Jonathan Wesley Stone <js@piratejon.com>
2018-11-24 12:48:03 -0600

    satellite alias

--- birdplans.py
+++ birdplans.py
@@ -25,6 +25,10 @@ class TestBirdplan(unittest.TestCase):
         # check initialization stuff
         self.assertEqual(plan.tle_file, 'data/tle/amateur.txt')
         self.assertTrue('SO-50' in plan.tle)
+        self.assertTrue('FOX-1B' in plan.tle)
+        self.assertFalse('AO-91' in plan.tle)
+        plan.add_satellite_alias('FOX-1B', 'AO-91')
+        self.assertTrue('AO-91' in plan.tle)
 
         # find next 24hrs passes
         results = plan.query_point_in_time(
@@ -35,10 +39,6 @@ class TestBirdplan(unittest.TestCase):
         self.assertAlmostEqual(results.alt.degrees, 26.71944, places=5)
         self.assertAlmostEqual(results.azimuth.degrees, 196.69795, places=5)
         self.assertAlmostEqual(results.distance, 1244.45630, places=5)
-
-        results = plan.query_pass(
-            'SO-50', 'EM15ek', plan.timescale.utc(2018, 11, 19, 0, 0, 0), 24, 15
-        )
 
 class BirdplanResults:
     '''results of a birdplan query'''
@@ -53,6 +53,27 @@ def f_altitude(bird, topos, time_t):
     '''calculate the altitude of bird with respect to topos at time_t'''
     (bird - topos).at(time_t).topocentric.altaz()[0]
 
+class PassQuery:
+    '''
+    try this:
+    <https://github.com/skyfielders/astronomy-notebooks/blob/master/Solvers/Earth-Satellite-Passes.ipynb>
+    as accessed 2018-11-19
+
+    encapsulated to return a number of values to the user
+    '''
+
+    def __init__(self, birdplan, satellite, location, timerange):
+        '''Predict passings of a satellite over a point on the earth during a given time window.
+
+        :param birdplan: a Birdplan object containing a timescale and 
+        :param satellite: the name of a satellite to lookup in birdplan.tle
+        :param location: the Topos object 
+        :param timerange: a Time array under which to find passes
+        '''
+        self.birdplan = birdplan
+        self.location = location
+        self.timerange = timerange
+
 class Birdplan:
     '''interface for bird planning'''
 
@@ -62,32 +83,12 @@ class Birdplan:
         self.tle = load.tle(tle_file)
         self.timescale = load.timescale()
 
+    def add_satellite_alias(self, satellite, alias):
+        self.tle[alias] = self.tle[satellite]
+
     def query_point_in_time(self, bird, maidenhead, when):
         '''find the altitude, azimuth, and distance of bird with respect to maidenhead at time'''
         where = mh.toLoc(maidenhead)
         diff = self.tle[bird] - Topos(*where)
         topocentric = diff.at(when)
         return BirdplanResults(where, topocentric)
-
-    def query_pass(bird, maidenhead, start, dur_hrs, min_alt):
-        '''
-        try this:
-        <https://github.com/skyfielders/astronomy-notebooks/blob/master/Solvers/Earth-Satellite-Passes.ipynb>
-        as access 2018-11-19
-        '''
-        where = mh.toLoc(maidenhead)
-        diff = self.tle[bird] - Topos(*where)
-
-        orbit_period_per_minute = (2.0 * np.pi) / self.tle[bird].model.no
-        orbit_period = orbit_period_per_minute / 24.0 / 60.0
-        revolutions_per_day = 1.0 / orbit_period
-        step = orbit_period / 6
-        t0 = start.tai
-        t = t0 + np.arange(0, 1.0, step)
-        dt = JulianDate(tai=t).utc_datetime()
-        altitudes = f_altitude(self.tle[bird], Topos(*where), dt) # diff.at(dt).topocentric.altaz()[0]
-        left_diff = np.ediff1d(altitudes, to_begin=0.0)
-        right_diff = np.ediff1d(altitudes, to_end=0.0)
-        maxima = (left_diff > 0.0) & (right_diff < 0.0)
-
-        result = optimize.minimize_scalar(lambda t: -f_altitude(t), bracket=[t + step, t - step], tol=one_second / t)
