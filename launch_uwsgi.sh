bdda241ceeed9859d72a70a1e54a8d61a5227ca2
Jonathan Wesley Stone <js@piratejon.com>
2018-12-03 18:28:10 -0600

    pass tip to script; launch 8 processes

--- launch_uwsgi.sh
+++ launch_uwsgi.sh
@@ -1,1 +1,4 @@
-uwsgi --http :9090 --wsgi-file birdplans.py -M
+#!/bin/bash
+
+tip=`git rev-parse HEAD 2>/dev/null || echo none`
+uwsgi --http :9090 --wsgi-file birdplans.py --master --processes 8 --pyargv $tip
