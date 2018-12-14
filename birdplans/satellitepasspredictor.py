07f08d202a4b5f04825570385b0cec5d145ed319
Jonathan Wesley Stone <js@piratejon.com>
2018-12-13 22:16:13 -0600

    remove unused minimum_altitude; printfs

--- birdplans/satellitepasspredictor.py
+++ birdplans/satellitepasspredictor.py
@@ -21,7 +21,7 @@ from scipy import optimize
 Pass = namedtuple('Pass', ['AOS', 'TCA', 'LOS'])
 WindowPasses = namedtuple('WindowPasses', ['diff', 'passes'])
 
-def estimate_window_passes(timescale, satellite, location, window_start, window_end, minimum_altitude):
+def estimate_window_passes(timescale, satellite, location, window_start, window_end):
     '''
     Implement the satellite pass prediction approach from
     <https://github.com/skyfielders/astronomy-notebooks/blob/master/Solvers/Earth-Satellite-Passes.ipynb>
@@ -39,33 +39,23 @@ def estimate_window_passes(timescale, satellite, location, window_start, window_
     diff = satellite - location
     window_duration = window_end - window_start
 
-    print('window_start', window_start)
-    print('window_end', window_end)
-    print('window_duration', window_duration)
-
     orbit_period_per_day = ((2.0 * np.pi) / satellite.model.no) / 24.0 / 60.0
     window_revolutions = window_duration / orbit_period_per_day
     sample_points = int(math.ceil(window_revolutions * 6.0))
-    print('sample_points', sample_points)
     sample_step = window_duration / sample_points
-    print('sample_step', sample_step)
 
     sample_time_range = timescale.tai_jd([
         window_start.tai + (_ * sample_step)
         for _ in range(sample_points)
     ])
-    print('sample_time_range', sample_time_range)
 
     alt_f = lambda t: diff.at(t).altaz()[0].degrees
 
     sample_altitudes = alt_f(sample_time_range)
-    print(sample_altitudes)
 
     left_diff = np.ediff1d(sample_altitudes, to_begin=0.0)
     right_diff = np.ediff1d(sample_altitudes, to_end=0.0)
     maxima = (left_diff > 0.0) & (right_diff < 0.0)
-
-    print(sample_time_range[maxima])
 
     minus_alt_f_wrapper = lambda t: -alt_f(timescale.tai_jd(t))
     find_peaks = lambda t: optimize.minimize_scalar(
@@ -74,13 +64,9 @@ def estimate_window_passes(timescale, satellite, location, window_start, window_
         , tol=(1.0 / 24.0 / 60.0 / 60.0) / t.tai
     ).x
 
-    np.random.seed(99999)
     t_peaks = timescale.tai_jd([
         find_peaks(ti) for ti in sample_time_range[maxima]
     ])
-
-    print(sample_step)
-    print(t_peaks)
 
     rising_setting_wrapper = lambda t: -minus_alt_f_wrapper(t)
     find_rising = lambda t: optimize.brentq(
@@ -90,7 +76,10 @@ def estimate_window_passes(timescale, satellite, location, window_start, window_
         rising_setting_wrapper, t.tai + (2.0 * sample_step), t.tai
     )
 
-    pass_times = [timescale.tai_jd([find_rising(_), _.tai, find_setting(_)]) for _ in t_peaks if alt_f(_) > 0]
+    pass_times = [
+        Pass(*timescale.tai_jd([find_rising(_), _.tai, find_setting(_)]))
+        for _ in t_peaks if alt_f(_) > 0
+    ]
 
     return WindowPasses(diff, pass_times)
 
@@ -123,13 +112,12 @@ def pass_estimation_wrapper(
         , Topos(*mh.toLoc(grid))
         , time_range[0]
         , time_range[-1]
-        , minimum_altitude
     )
 
     return WindowPasses(
         all_passes.diff
         , [
             _pass for _pass in all_passes.passes
-            if all_passes.diff.at(_pass[1]).altaz()[0].degrees >= minimum_altitude
+            if all_passes.diff.at(_pass.TCA).altaz()[0].degrees >= minimum_altitude
         ]
     )
