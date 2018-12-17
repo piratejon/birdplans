681eda12618a5ca9501bb85294a949d773d741ca
Jonathan Wesley Stone <js@piratejon.com>
2018-12-17 17:53:56 -0600

    launcher uses new uwsgi script

--- launch_uwsgi.sh
+++ launch_uwsgi.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 tip=`git rev-parse HEAD 2>/dev/null || echo none`
-uwsgi --http :9090 --wsgi-file birdplans.py --master --processes 8 --pyargv $tip --safe-pidfile ./pidfile.txt
+uwsgi --http :9090 --wsgi-file birdplans/uwsgi.py --master --processes 8 --pyargv $tip --safe-pidfile ./pidfile.txt
